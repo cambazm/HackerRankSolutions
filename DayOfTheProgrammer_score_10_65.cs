7f45dab267ae544e40935cc7e4eb6cd49935813d
cambazm <56502154+cambazm@users.noreply.github.com>
2020-02-12 15:18:52 +0400

    Add files via upload

---
+++ DayOfTheProgrammer_score_10_65.cs
@@ -0,0 +1,40 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the dayOfProgrammer function below.
+    static string dayOfProgrammer(int year) {
+        DateTime d;
+        if(year <= 1917)
+            d = new DateTime(year,1,1).AddDays(254);
+        else
+            d = new DateTime(year,1,1).AddDays(255);
+
+        return d.ToString("dd.MM.yyyy");
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int year = Convert.ToInt32(Console.ReadLine().Trim());
+
+        string result = dayOfProgrammer(year);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
